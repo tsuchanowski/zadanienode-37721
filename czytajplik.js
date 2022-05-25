5c0d7c74f4cf40ea4edaa92cb7528b72773f71bb
tsuchanowski <104353564+tsuchanowski@users.noreply.github.com>
2022-05-25 20:37:00 +0200

    wersja ostateczna-poprawiona

--- czytajplik.js
+++ czytajplik.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 const path = require("path");
 
 function saveData(sciezka, nazwa, nadpisac) {
-  
+
   sciezka = path.join(__dirname, "dane");
-  
+
   fs.readdir(path.join(__dirname, "dane"), function (err, files) {
     if (err) {
       console.log(err);
@@ -13,50 +13,50 @@ function saveData(sciezka, nazwa, nadpisac) {
       console.log(__dirname);
 
       files.forEach(function (file) {
-        fs.readFile(path.join(__dirname, "dane", file),"utf-8", function (err, data) {
-            if (err ) {
-              console.log(err);
-            } else {
-              fs.mkdir(path.join(__dirname, nazwa), function (err) {
-                if (err && !nadpisac) {
-                  console.log(err );
-                  console.log("Folder już istnieje");
-                } else {
-                  console.log("Stworzono nowy folder");
+        fs.readFile(path.join(__dirname, "dane", file), "utf-8", function (err, data) {
+          if (err) {
+            console.log(err);
+          } else {
+            fs.mkdir(path.join(__dirname, nazwa), function (err) {
+              if (err && !nadpisac) {
+                console.log(err);
+                console.log("Folder już istnieje");
+              } else {
+                console.log("Stworzono nowy folder");
 
-                  var id = "";
-                  var name = "";
-                  var obiekt = JSON.parse(data);
-                  var dlugosc = obiekt.length;
+                let obiekt = JSON.parse(data);
+                let dlugosc = obiekt.length;
 
-                  while (dlugosc--) {
-                    var id = obiekt[dlugosc].id;
+                while (dlugosc--) {
+                  let id = obiekt[dlugosc].id;
 
-                    var name = obiekt[dlugosc].name;
-                    let fileData =
-                      "name: " +
-                      obiekt[dlugosc].name +
-                      "\n street: " +
-                      obiekt[dlugosc].address.street +
-                      "\n Zip Code: " +
-                      obiekt[dlugosc].address.zipcode +
-                      "\n City: " +
-                      obiekt[dlugosc].address.city +
-                      "\n Phone: " + 
-                      obiekt[dlugosc].phone;
+                  let name = obiekt[dlugosc].name;
+                  let fileData =
+                    "name: " +
+                    obiekt[dlugosc].name +
+                    "\n street: " +
+                    obiekt[dlugosc].address.street +
+                    "\n Zip Code: " +
+                    obiekt[dlugosc].address.zipcode +
+                    "\n City: " +
+                    obiekt[dlugosc].address.city +
+                    "\n Phone: " +
+                    obiekt[dlugosc].phone;
 
-                    fs.writeFile(path.join(__dirname, nazwa, "id-" + id + " -" + name + ".txt"), fileData, function (err) {
-                        if (err.code === "EEXIST") {
-                          console.log("Plik już istnieje");
-                          return;
-                        }
-                      }
-                    );
+                  fs.writeFile(path.join(__dirname, nazwa, "id-" + id + " -" + name + ".txt"), fileData, function (err) {
+                    if (err)
+                      console.log(err);
+                    else {
+                      console.log("File written successfully\n");
+                    }
+
                   }
+                  );
                 }
-              });
-            }
+              }
+            });
           }
+        }
         );
       });
     }
@@ -64,8 +64,8 @@ function saveData(sciezka, nazwa, nadpisac) {
   return sciezka;
 }
 
- module.exports = {
-               saveData: saveData
+module.exports = {
+  saveData: saveData
 }
 
 
